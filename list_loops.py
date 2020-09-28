beaadb1180e2f4f74962e933da5098fb497353f8
Gobind Puniani <gobindpuniani@gmail.com>
2020-09-28 12:01:52 -0700

    Created Python file and finished first two tasks

---
+++ list_loops.py
@@ -0,0 +1,13 @@
+#Q1
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+
+#Q2
+first_song = songs[0]
+print(f'The first song of the list: "{first_song}"')
+
+last_song = songs[-1]
+print(f'The last song of the list: "{last_song}"')
+
+
+
